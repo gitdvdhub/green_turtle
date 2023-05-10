3c1c5b3f687ebe277ddf9c16ecf98598963675fc
lordaval <lordaval1117@gmail.com>
2023-05-10 23:09:40 +0000

    aula 8-9

--- projetos/meu-projeto/src/App.js
+++ projetos/meu-projeto/src/App.js
@@ -1,25 +1,26 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
-  const name = "Matheus";
-
-  function sum(a, b) {
-    return a + b;
-  }
-
-  const url = "https://via.placeholder.com/150";
+  const nome = "Maria"
 
   return (
     <div className="App">
-      <h2>Alterando o JSX</h2>
-      <p>Olá, {name}!</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="Minha imagem" />
-      <HelloWorld />
-      <HelloWorld />
-      <HelloWorld />
+      <h1>Teste</h1>
+      <Frase />
+      <Frase />
+      <SayMyName nome="Matheus" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa 
+        nome="Rodrigo"
+        idade="28"
+        profissao="Programador"
+        foto="https://via.placeholder.com/150"
+        />
     </div>
   );
 }
--- projetos/meu-projeto/src/components/Frase.js
+++ projetos/meu-projeto/src/components/Frase.js
@@ -1,7 +1,10 @@
+import styles from './Frase.module.css';
+
 function Frase() {
     return (
-        <div>
+        <div className={styles.fraseContainer}>
             
+            <p className={styles.fraseContent}>Este é um componente com uma frase!</p>
         </div>
     );
 }
