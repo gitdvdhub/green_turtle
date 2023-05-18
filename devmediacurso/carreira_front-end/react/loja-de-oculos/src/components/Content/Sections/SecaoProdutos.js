90b3f83820f8f0c28e211d617c7b229f651d8d0d
lordaval <lordaval1117@gmail.com>
2023-05-18 12:54:09 +0000

    criaçao da seçao de produtos

---
+++ devmediacurso/carreira_front-end/react/loja-de-oculos/src/components/Content/Sections/SecaoProdutos.js
@@ -0,0 +1,11 @@
+function SecaoProdutos() {
+    return (
+        <section className="secao-produtos">
+            <div>
+                
+            </div>
+        </section>
+    );
+}
+
+export default SecaoProdutos;
