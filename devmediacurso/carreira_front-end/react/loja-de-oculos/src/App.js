e640f0499c09f2409030c9a914025d04b15ed000
lordaval <lordaval1117@gmail.com>
2023-05-22 13:42:46 +0000

    footer finalizado

--- devmediacurso/carreira_front-end/react/loja-de-oculos/src/App.js
+++ devmediacurso/carreira_front-end/react/loja-de-oculos/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Content/Main';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
---
+++ devmediacurso/carreira_front-end/react/loja-de-oculos/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-content">
+                <p>&copy; 2022 Óticas Vida - Todos os direitos reservados</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
