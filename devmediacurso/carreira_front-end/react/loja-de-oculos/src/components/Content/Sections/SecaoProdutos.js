724f0dc725339ceefd8aa0e5011d139ca1a56b63
lordaval <lordaval1117@gmail.com>
2023-05-22 12:55:14 +0000

    SecaoProdutos finalizada

--- devmediacurso/carreira_front-end/react/loja-de-oculos/src/components/Content/Sections/SecaoProdutos.js
+++ devmediacurso/carreira_front-end/react/loja-de-oculos/src/components/Content/Sections/SecaoProdutos.js
@@ -1,8 +1,40 @@
+
 function SecaoProdutos() {
     return (
         <section className="secao-produtos">
-            <div>
-                
+            <div className="secao-produtos-content">
+                <h2>NOSSOS PRODUTOS</h2>
+                <p>Trabalhamos com óculos de grau, óculos de sol, lentes transition nos modelos masculino, feminino e infantil.</p>
+                <p>Todos os nossos preços são acessíveis e contam com a melhor qualidade do mercado.</p>
+                <div className="cards">
+                    <div className="card">
+                        <h2>Óculos de grau</h2>
+                        <img src={process.env.PUBLIC_URL + "/assets/images/oculos01.png"} alt="Óculos de grau 1" />
+                        <p>R$ 500,00</p>
+                    </div>
+                    <div className="card">
+                        <h2>Óculos de sol</h2>
+                        <img src={process.env.PUBLIC_URL + "/assets/images/oculos02.png"} alt="Óculos de sol 2" />
+                        <p>R$ 700,00</p>
+                    </div>
+                    <div className="card">
+                        <h2>Lentes transition</h2>
+                        <img src={process.env.PUBLIC_URL + "/assets/images/oculos03.png"} alt="Lentes transition" />
+                        <p>R$ 750,00</p>
+                    </div>
+                    <div className="card">
+                        <h2>Óculos infantil</h2>
+                        <img src={process.env.PUBLIC_URL + "/assets/images/oculos04.png"} alt="Óculos infantil" />
+                        <p>R$ 500,00</p>
+                    </div>
+                </div>
+                <p>Todos os nossos produtos incluem:</p>
+                <ul>
+                    <li>Garantia de 1 ano</li>
+                    <li>Manutenção preventiva</li>
+                    <li>Descontos especiais na compra da segunda unidade</li>
+                    <li>Flexibilidade de pagamento</li>
+                </ul>
             </div>
         </section>
     );
