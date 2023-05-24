8885bad16e2950071a561cec0159d924e951f5c7
lordaval <lordaval1117@gmail.com>
2023-05-24 11:42:24 +0000

    rotas

---
+++ devmediacurso/carreira_front-end/react/rotas/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Rotas from './components/Rotas';
+import PaginaInicial from './components/PaginaInicial';
+
+const App = () => (
+  <div className="App">
+    <Rotas />
+    <PaginaInicial />
+  </div>
+);
+
+export default App;
